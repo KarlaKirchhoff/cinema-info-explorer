d88b89f1b6c32dad0eac8aae4444a5331324d03f
Karla Kirchhoff <keikheyk@gmail.com>
2026-01-09 20:39:10 -0300

    ajustando configurações variaveis e ambiente da api

---
+++ src/api/index.ts
@@ -0,0 +1,21 @@
+import { TMDB_API_TOKEN, TMDB_API_KEY, TMDB_BASE_URL } from '@env';
+
+export function DefaultApiOptionRequest(method: string = 'GET'){
+    const opt_method = method.toUpperCase()
+    if(opt_method !== 'GET' ){
+        throw new Error('Método Inválido.\n Opções Válidas: GET')
+    }
+
+    return {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${TMDB_API_TOKEN}`,
+      },
+    };
+}
+
+export const urlBase_Apikey = {
+    url: TMDB_BASE_URL,
+    key: TMDB_API_KEY
+}
