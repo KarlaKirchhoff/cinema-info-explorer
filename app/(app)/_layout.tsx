cf8b1a0623d758b818c870d25a5de80176ab179e
Karla Kirchhoff <keikheyk@gmail.com>
2026-01-15 19:20:30 -0300

    feat: Implementado layout e componente TopBar com navegação e troca de tema

---
+++ app/(app)/_layout.tsx
@@ -0,0 +1,20 @@
+// app/(app)_layout.tsx
+import { Slot } from 'expo-router';
+import TopBar from '../../src/components/NavigationComponents/TopBar';
+import { View, StyleSheet } from 'react-native';
+import { useTheme } from '../../src/context/themeContext';
+
+export default function AppLayout() {
+  const { colors } = useTheme();
+
+  return (
+    <View style={[styles.container, { backgroundColor: colors.background }]}>
+      <TopBar />
+      <Slot />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+});
